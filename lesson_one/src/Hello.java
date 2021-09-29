fc8d6b8fea4522e43aab0fec6f2145338de01ff4
Aaron Janes <aaron.janes@gmail.com>
2021-09-28 19:20:47 -0600

    my first java variable

--- lesson_one/src/Hello.java
+++ lesson_one/src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello world it is me again");
+        int myFirstNumber = 5;
+
+        System.out.println(myFirstNumber);
     }
 }
